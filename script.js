9a70e05a4c9466e08e4b3a6a91a8d6a705626cdd
thompsonslee <thompson.slee@hotmail.co.uk>
2023-01-24 13:39:59 +1100

    add code to change scoring indicators based on score

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 Math.random()
 let computerhand 
-console.log(computerhand);
+console.log(computerhand)
 
 const rock = document.getElementById("rock")
 const paper = document.getElementById("paper")
@@ -8,13 +8,13 @@ const scissors = document.getElementById("scissors")
 
 rock.addEventListener("click", function(){
     checkgame(1)
-})
+});
 paper.addEventListener("click", function(){
     checkgame(2)
-})
+});
 scissors.addEventListener("click", function(){
     checkgame(3)
-})
+});
 
 
 function checkgame(playerhand){
@@ -23,42 +23,66 @@ function checkgame(playerhand){
 
     if (computerhand === 1){
         if (playerhand === 1){
-            alert ("Tie!")
+            scoring("tie")
         }
         else if (playerhand === 2){
-            alert ("Player Wins!")
+            scoring("playerwin")
         }
         else if (playerhand === 3){
-            alert("Computer Wins!")
+            scoring("computerwin")
         }
     }
 
 
     else if (computerhand === 2){
         if (playerhand === 1){
-            alert("Computer Wins!")
+            scoring("computerwin")
         }
         else if (playerhand === 2){
-            alert("Tie!")
+            scoring("tie")
         }
         else if (playerhand === 3){
-            alert("Player Wins!")
+            scoring("playerwin")
         }
     }
 
 
     else if (computerhand === 3){
         if (playerhand === 1){
-            alert("Player Wins!")
+            scoring("playerwin")
         }
         else if (playerhand === 2){
-            alert("Computer Wins!")
+            scoring("computerwin")
         }
         else if (playerhand === 3){
-            alert("Tie!")
+            scoring("tie")
         }
     }
 }
+let computerscore = 0
+let playerscore = 0
+function scoring(result){
+    if (computerscore === 5){
+        alert("computer wins")
+    }
+    else if (playerscore ===5){
+        alert("player wins")
+    }
+    //if no one has won, run code below
+    else if (result === "playerwin"){
+            playerscore += 1
+            document.getElementById("playerscore").innerHTML = playerscore
+        }
+    else if (result === "computerwin"){
+            computerscore += 1
+            document.getElementById("computerscore").innerHTML = computerscore
+        }
+    else if (result === "tie"){ 
+            return ("tie")
+        }
+        console.log (computerscore)
+        console.log (playerscore)
+    }
 
 
 
